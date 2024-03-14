c900b70c2d09cd633daaa0d2e2e57da374938d2e
Haryo Waskito <haryo.waskito@banksinarmas.com>
2024-03-14 21:55:29 +0700

    End of section 14

--- API/Controllers/ActivitiesController.cs
+++ API/Controllers/ActivitiesController.cs
@@ -2,10 +2,10 @@ using Application.Activities;
 using Domain;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using SQLitePCL;
 
 namespace API.Controllers
 {
-    [AllowAnonymous]
     public class ActivitiesController : BaseApiController
     {
         [HttpGet] //api/activities
@@ -22,10 +22,11 @@ namespace API.Controllers
 
         [HttpPost]
         public async Task<IActionResult> CreateActivity(Activity activity)
-        {            
+        {
             return HandleResult(await Mediator.Send(new Create.Command { Activity = activity }));
         }
 
+        [Authorize(Policy = "IsActivityHost")]
         [HttpPut("{id}")]
         public async Task<IActionResult> EditActivity(Guid id, Activity activity)
         {
@@ -33,10 +34,18 @@ namespace API.Controllers
 
             return HandleResult(await Mediator.Send(new Edit.Command { Activity = activity }));
         }
+
+        [Authorize(Policy = "IsActivityHost")]
         [HttpDelete("{id}")]
         public async Task<IActionResult> DeleteActivity(Guid id)
         {
             return HandleResult(await Mediator.Send(new Delete.Command { Id = id }));
+        }
+
+        [HttpPost("{id}/attend")]
+        public async Task<IActionResult> Attend(Guid id)
+        {
+            return HandleResult(await Mediator.Send(new UpdateAttendance.Command { Id = id }));
         }
 
     }
--- API/Extentions/IdentityServiceExtensions.cs
+++ API/Extentions/IdentityServiceExtensions.cs
@@ -1,7 +1,10 @@
+using System.Net.Http.Headers;
 using System.Text;
 using API.Services;
 using Domain;
+using Infrastructure.Security;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.IdentityModel.Tokens;
 using Persistence;
 
@@ -31,6 +34,14 @@ namespace API.Extentions
                         ValidateAudience = false
                     };
                 });
+
+            services.AddAuthorization(opt => {
+               opt.AddPolicy("IsActivityHost", policy => 
+               {
+                policy.Requirements.Add(new IsHostRequirement());
+               });
+            });
+            services.AddTransient<IAuthorizationHandler, IsHostRequirementHandler>();
             services.AddScoped<TokenService>();
 
             return services;
