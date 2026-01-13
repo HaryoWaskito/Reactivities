5ca162c5d349ff5e7a6baddf64222fca18a7202d
Haryo Waskito <haryo.waskito@banksinarmas.com>
2026-01-13 08:23:04 +0700

    End of Section 3

--- API/Program.cs
+++ API/Program.cs
@@ -9,10 +9,15 @@ builder.Services.AddDbContext<AppDbContext>(opt =>
 {
     opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
+builder.Services.AddCors();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+app.UseCors(x => x.AllowAnyHeader()
+                  .AllowAnyMethod()
+                  .WithOrigins("http://localhost:3000", "https://localhost:3000"));
+
 app.MapControllers();
 
 using var scope = app.Services.CreateScope();
@@ -27,7 +32,7 @@ try
 catch (Exception ex)
 {
     var logger = services.GetRequiredService<ILogger<Program>>();
-    logger.LogError(ex,"An error occurred during migration");
+    logger.LogError(ex, "An error occurred during migration");
 }
 
 app.Run();
